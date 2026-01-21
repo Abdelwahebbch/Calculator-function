f4a45f0249e6ef43dbe512ea5df306abfcae7437
Abdelwaheb <bouchahwaabdelwaheb@fsg.u-gabes.tn>
2026-01-21 23:54:36 +0100

    modf

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
-def main(context):
-    name = context.req.query.get("name", "World")
-
-    return context.res.json({
-        "message": f"Hello {name}!",
-        "method": context.req.method,
-        "headers": context.req.headers
-    })
+def main() : 
+    return "Hello Abdelwaheb"
