2279d45d0ea22c2e470a8779eb92e160f442c6a2
Abdelwaheb <bouchahwaabdelwaheb@fsg.u-gabes.tn>
2026-01-22 00:05:39 +0100

    modification

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-def main() : 
-    return "Hello Abdelwaheb"
+import json
+
+def main(context):
+    return context.res.json({
+        "message": "Hello from Appwrite Python Function 🚀",
+        "method": context.req.method,
+        "body": context.req.body
+    })
