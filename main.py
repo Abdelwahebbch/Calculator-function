dfe5cfa3bcdc520f1366d73976dde4a2fe45c5d5
Abdelwaheb <bouchahwaabdelwaheb@fsg.u-gabes.tn>
2026-01-22 15:38:43 +0100

    update main

--- main.py
+++ main.py
@@ -2,36 +2,7 @@ import json
 
 
 def main(context):
-    try:
-        # Read body safely
-        body_raw = context.req.body or "{}"
-        body = json.loads(body_raw)
-
-        a = body.get("a")
-        b = body.get("b")
-
-        # Validate input
-        if a is None or b is None:
-            return context.res.json(
-                {"error": "Please provide both a and b"},
-                status=400
-            )
-
-        # Convert to numbers (VERY IMPORTANT)
-        a = float(a)
-        b = float(b)
-
-        result = a + b
-
-        return context.res.json({
-            "a": a,
-            "b": b,
-            "result": result
+    if (context.req.method == 'GET'): 
+        return json.decoder({
+            "Name":"Abdelwaheb"
         })
-
-    except Exception as e:
-        # Always return error details for debugging
-        return context.res.json(
-            {"error": str(e)},
-            status=500
-        )
